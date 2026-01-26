5f0ba4923c03d1964b65d2353da3992798f385bf
Radhe rai <radheshyamrai1547@gmail.com>
2026-01-26 22:34:58 +0530

    Add files via upload

---
+++ subtaction.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;       //initialization
+
+    cout<<"enter a:";  //input
+    cin>>a;
+    cout<<"enter b:";
+    cin>>b; 
+
+    int subtraction=a-b;
+    cout<<"subtraction is:"<<subtraction<<endl;
+
+    return 0;
+
+}
