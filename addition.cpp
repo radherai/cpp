8f4a047afefe79481e358e190083be067811628e
Radhe rai <radheshyamrai1547@gmail.com>
2026-01-26 22:46:05 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;       //initialization
+
+    cout<<"enter a:";  //input
+    cin>>a;
+    cout<<"enter b:";
+    cin>>b; 
+
+    int add=a+b;
+    cout<<"additon is:"<<add<<endl;
+
+    return 0;
+
+}
