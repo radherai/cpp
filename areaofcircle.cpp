e6556f3163b3d6fe31340a6110116d014cdf288e
Radhe rai <radheshyamrai1547@gmail.com>
2026-01-28 18:55:36 +0530

    Add files via upload

---
+++ areaofcircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    float pi=3.14;
+    cout<<"enter radius r:";
+    cin>>r;
+    float area=pi*r*r;
+    cout<<"area of circle is:"<<area;
+    return 0;
+
+}
