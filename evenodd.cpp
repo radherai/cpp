d8da744563ffbe42411ea8f68dabaaf381513667
Radhe rai <radheshyamrai1547@gmail.com>
2026-01-31 00:26:07 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"x:";
+    cin>>x;
+    if(x%2==0){
+        cout<<"x is even";
+    }
+    else
+    cout<<"x is odd";
+    return 0;
+}
+  
+  
